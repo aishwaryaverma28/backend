95b9c7bde45c640d1df93b5a414835213df6b8a0
aishwaryaverma28 <aishwarya.verma28@gmail.com>
2024-04-19 14:26:33 +0530

    start of backend

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/login', (req, res) => {
+    res.send('<h1>Please login</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
